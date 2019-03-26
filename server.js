edbaea1197cd36d65979f2d50282f8235e325a9b
Andrew Kolb <akolb87@gmail.com>
2019-03-25 23:31:02 -0500

    dependancy setup

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static('public'));
+require('dotenv').config();
 
 const cors = require('cors');
-const { CLIENT_ORIGIN } = require('./config');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const { PORT, DATABASE_URL } = require('./config');
 
 app.use(
     cors({
@@ -12,10 +16,46 @@ app.use(
     })
 );
 
-app.get('/api/*', (req, res) => {
-    res.json({ ok: true });
-});
+// app.get('/api/*', (req, res) => {
+//     res.json({ ok: true });
+// });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
-module.exports = { app };
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer }
