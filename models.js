0fd29eb1d51c2d6622ea45bf011d0650074c1d1c
Andrew Kolb <akolb87@gmail.com>
2019-03-25 23:38:25 -0500

    model init

---
+++ models.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { PORT, DATABASE_URL } = require('./config');
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+require('dotenv').config();
+
+const savedBuildSchema = mongoose.Schema({
+  filler: { type: String, required: true },
+  filler2: { type: String, required: true },
+  filler3: { type: String, required: true },
+  filler4: { type: Number, required: true },
+});
+
+characterSchema.methods.serialize = function () {
+  return {
+    id: this._id,
+    filler: this.filler,
+    filler2: this.filler2,
+    filler3: this.filler3,
+    filler4: this.filler4,
+  };
+};
+
+const Build = mongoose.model('Build', savedBuildSchema);
+
+module.exports = { Build };
