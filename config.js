8e1ffa4acc63d5a1b2888053ab4d5cf18a0a568a
Andrew Kolb <akolb87@gmail.com>
2019-03-30 08:40:06 -0500

    config update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 require('dotenv').config();
+exports.DATABASE_URL = process.env.DATABASE_URL;
+// included login sample just to show test suite is working for project submission
+// exports.TEST_DATABASE_URL = ;
+exports.PORT = process.env.PORT || 8080;
 
-module.exports = {
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-    DATABASE_URL = process.env.DATABASE_URL;
-    PORT = process.env.PORT || 8080;
-};
+exports.CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
